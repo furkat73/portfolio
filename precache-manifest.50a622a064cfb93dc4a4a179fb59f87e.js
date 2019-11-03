8e1b8a1415ae01a92f7a9ffc80e8978ba09c196a
furkat73 <fkurganbaev@gmail.com>
2019-11-03 16:40:59 +0500

    Updates

---
+++ precache-manifest.50a622a064cfb93dc4a4a179fb59f87e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a74b666cea339b42d760d32b9ecd0f1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4032f0623325d66b608a",
+    "url": "/portfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "2df3dc7e2f67b74fcc5a",
+    "url": "/portfolio/static/css/main.121eb549.chunk.css"
+  },
+  {
+    "revision": "4032f0623325d66b608a",
+    "url": "/portfolio/static/js/2.279dc6b0.chunk.js"
+  },
+  {
+    "revision": "2df3dc7e2f67b74fcc5a",
+    "url": "/portfolio/static/js/main.f1d5b81f.chunk.js"
+  },
+  {
+    "revision": "e181ba878306554881df",
+    "url": "/portfolio/static/js/runtime-main.f2d1ac2f.js"
+  },
+  {
+    "revision": "898e1637a477385d11abaa28253736ba",
+    "url": "/portfolio/static/media/avatar.898e1637.png"
+  }
+]);
